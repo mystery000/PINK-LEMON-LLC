b2f117140eb73d8543761eafbf1dc4466773163c
arthlo <lkostyrka17@gmail.com>
2023-06-14 22:18:50 +0000

    Create Spring project

---
+++ pink-lemon-llc-backend/src/main/java/com/pinklemonllc/pinklemonllcbackend/PinkLemonLlcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pinklemonllc.pinklemonllcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkLemonLlcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinkLemonLlcBackendApplication.class, args);
+	}
+
+}
---
+++ pink-lemon-llc-backend/src/test/java/com/pinklemonllc/pinklemonllcbackend/PinkLemonLlcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinklemonllc.pinklemonllcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinkLemonLlcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
