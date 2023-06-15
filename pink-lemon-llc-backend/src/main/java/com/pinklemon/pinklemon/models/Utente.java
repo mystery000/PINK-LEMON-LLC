63b51261615f26241ce92d967f2456673d9d7ca1
arthlo <lkostyrka17@gmail.com>
2023-06-15 22:47:38 +0200

    Create Utente data model

--- pink-lemon-llc-backend/src/main/java/com/pinklemon/pinklemon/models/Utente.java
+++ pink-lemon-llc-backend/src/main/java/com/pinklemon/pinklemon/models/Utente.java
@@ -5,6 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Table;
 import org.springframework.data.annotation.Id;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import java.util.Date;
 
 @Entity
 @Table(name="Utentes")
@@ -28,14 +31,23 @@ public class Utente {
     @Column (unique = true, nullable = false)
     private String email;
 
-    @Column(nullable = true)
-    private int credito;
+    @Column
+    private boolean deleted;
+
+    @Column
+    private int role;
+
+    @Column
+    private Date created_time;
 
     @Column(nullable = true)
-    private int credito_mensile;
+    private int credit;
 
     @Column(nullable = true)
-    private int credito_annuale;
+    private int credit_monthly;
+
+    @Column(nullable = true)
+    private int credit_annual;
 
     public void setId(Long id) {
         this.id = id;
@@ -43,5 +55,103 @@ public class Utente {
 
     public Long getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+    public Date getCreated_time() {
+        return created_time;
+    }
+
+    public void setCreated_time(Date created_time) {
+        this.created_time = created_time;
+    }
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+    public int getCredit_monthly() {
+        return credit_monthly;
+    }
+
+    public void setCredit_monthly(int credit_monthly) {
+        this.credit_monthly = credit_monthly;
+    }
+    public int getCredit_annual() {
+        return credit_annual;
+    }
+
+    public void setCredit_annual(int credit_annual) {
+        this.credit_annual = credit_annual;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+                .append("id", getId())
+                .append("name", getName())
+                .append("surname", getSurname())
+                .append("userName", getUsername())
+                .append("email", getEmail())
+                .append("password", getPassword())
+                .append("deleted", getDeleted())
+                .append("role", getRole())
+                .append("createTime", getCreated_time())
+                .append("credit", getCredit())
+                .append("role", getCredit_monthly())
+                .append("createTime", getCredit_annual())
+                .toString();
     }
 }
--- pink-lemon-llc-backend/src/main/resources/db/migration/CreateUtenteTable.sql
+++ pink-lemon-llc-backend/src/main/resources/db/migration/CreateUtenteTable.sql
@@ -1,6 +1,6 @@
 drop table if exists Utentes;
 create table Utentes (
-  user_id           bigint(20)      not null auto_increment    comment 'UserID',
+  id           bigint(20)      not null auto_increment    comment 'UserID',
   name              varchar(30)     default ''                 comment 'FirstName',
   surname           varchar(30)     default ''                 comment 'LastName',
   username          varchar(30)     not null                   comment 'UserName',
@@ -9,8 +9,11 @@ create table Utentes (
   role              smallint        default 0                  comment 'User Role (0: User 1: System User)',
   deleted           boolean         default false              comment 'Deleted User (true: Yes, false: No)',
   create_time       datetime                                   comment 'Created Time',
-  credito           int(4)          default 0                  comment 'Credit',
-  credito_mensile   int(4)          default 0                  comment 'Monthly Credit',
-  credito_annuale   int(4)          default 0                  comment 'Annual Credit',
-  primary key (user_id)
+  credit            int(4)          default 0                  comment 'Credit',
+  credit_monthly    int(4)          default 0                  comment 'Monthly Credit',
+  credit_annual     int(4)          default 0                  comment 'Annual Credit',
+  primary key (id)
 ) engine=innodb auto_increment=100 comment = 'User Information Form';
+
+
+insert into Utentes values(1, 'Mohamed', 'Fassel', 'ARTHLO', 'lkostyrka17@gmail.com', '$2a$10$7JB720yubVSZvUI0rEqK/.VqGOZTH.ulu33dHOiBE8ByOhJIrdAu2', 1, false, sysdate(), 200, 120, 1200)
