4180f9f56326ab944469e2f80fbaae310a99c8d9
arthlo <lkostyrka17@gmail.com>
2023-06-16 23:10:38 +0200

    Fixes commit

--- pink-lemon-llc-backend/src/main/java/com/pinklemon/pinklemon/PinklemonApplication.java
+++ pink-lemon-llc-backend/src/main/java/com/pinklemon/pinklemon/PinklemonApplication.java
@@ -2,6 +2,9 @@ package com.pinklemon.pinklemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PinklemonApplication {
@@ -10,5 +13,13 @@ public class PinklemonApplication {
 		SpringApplication.run(PinklemonApplication.class, args);
 		System.out.println("Server is running...");
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
---
+++ pink-lemon-llc-backend/src/main/java/com/pinklemon/pinklemon/config/SpringSecurityConfig.java
@@ -0,0 +1,12 @@
+package com.pinklemon.pinklemon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SpringSecurityConfig {
+
+
+}
