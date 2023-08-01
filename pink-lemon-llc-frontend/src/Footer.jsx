1df871e4a72d33d4012a926dfc2a1398150b4a98
Leonardo Fredduzzi <112616182+LeonardoFred@users.noreply.github.com>
2023-08-01 20:49:57 +0200

    Add files via upload

--- pink-lemon-llc-frontend/src/Footer.jsx
+++ pink-lemon-llc-frontend/src/Footer.jsx
@@ -1,29 +1,21 @@
 //import Logo from './Logo';
-import { NavLink } from 'react-router-dom';
+
 import './navbar.css';
 
 export default function Footer() {
     return (
         <>
             <div className="footbar-app">
-                <div className="nav">
+                <div className='footer-link'>
+              
                     {/** Abbiamo <Link> e <NavLink> (usiamo il secondo perché customizzabile)
                      *  il to="" corrisponde al mio link in href
                      * In pagina avrò <a class="navbar__item" href="/about">About</a>
                      */}
-                    <NavLink to="/" className="navbar-link">
-                        Home
-                    </NavLink>
-
-                    <NavLink to="/services" className="navbar-link">
-                        Services
-                    </NavLink>
-
-                    <NavLink to="/contacts" className="navbar-link">
-                        Contact
-                    </NavLink>
+                    PINK LEMON LLC - 3946, 1603 Capitol Avenue, Suite 413, Cheyenne, WY, Laramie, US, 82001. All rights reserved.
                 </div>
-            </div>
+                </div>
+           
         </>
     );
 }
